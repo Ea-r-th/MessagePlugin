3bee9ec033e7a0ad095303e2aab43633eaccad94
Ea-r-th <luca.lizaranzu@gmail.com>
2020-08-09 21:13:33 -0700

    Initial Commit

---
+++ src/main/java/me/earth/messageplugin/MessagePlugin.java
@@ -0,0 +1,16 @@
+package me.earth.messageplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MessagePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
